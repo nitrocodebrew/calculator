5074f765fa977505625bb78767730ba93b858585
John Smicklo <204639360+nitrocodebrew@users.noreply.github.com>
2026-06-24 16:06:56 -0400

    Implement doRenderKeypad function
    Generate calculator keypad; placeholder action functions

--- js/app.js
+++ js/app.js
@@ -27,3 +27,139 @@ const doClearScreen = () => {
     calculatorResult.textContent = '';
 };
 
+const doRenderKeypad = () => {
+    const keypad = {
+        backspace: {
+            value: '⌫',
+            action: doBackspace,
+            order: 1,
+            type: 'operator',
+        },
+        clear: {
+            value: 'AC',
+            action: doClearScreen,
+            order: 2,
+            type: 'operator',
+        },
+        remainder: {
+            value: '%',
+            action: () => {},
+            order: 3,
+            type: 'operator',
+        },
+        divide: {
+            value: '÷',
+            action: () => {},
+            order: 4,
+            type: 'operator',
+        },
+        7: {
+            value: 7,
+            action: () => {},
+            order: 5,
+            type: 'number',
+        },
+        8: {
+            value: 8,
+            action: () => {},
+            order: 6,
+            type: 'number',
+        },
+        9: {
+            value: 9,
+            action: () => {},
+            order: 7,
+            type: 'number',
+        },
+        multiply: {
+            value: '×',
+            action: () => {},
+            order: 8,
+            type: 'operator',
+        },
+        4: {
+            value: 4,
+            action: () => {},
+            order: 9,
+            type: 'number',
+        },
+        5: {
+            value: 5,
+            action: () => {},
+            order: 10,
+            type: 'number',
+        },
+        6: {
+            value: 6,
+            action: () => {},
+            order: 11,
+            type: 'number',
+        },
+        subtract: {
+            value: '-',
+            action: () => {},
+            order: 12,
+            type: 'operator',
+        },
+        1: {
+            value: 1,
+            action: () => {},
+            order: 13,
+            type: 'number',
+        },
+        2: {
+            value: 2,
+            action: () => {},
+            order: 14,
+            type: 'number',
+        },
+        3: {
+            value: 3,
+            action: () => {},
+            order: 15,
+            type: 'number',
+        },
+        add: {
+            value: '+',
+            action: () => {},
+            order: 16,
+            type: 'operator',
+        },
+        0: {
+            value: 0,
+            action: () => {},
+            order: 17,
+            type: 'number',
+        },
+        decimal: {
+            value: '.',
+            action: () => {},
+            order: 18,
+            type: 'number',
+        },
+        sign: {
+            value: '+/-',
+            action: () => {},
+            order: 19,
+            type: 'number',
+        },
+        equals: {
+            value: '=',
+            action: () => {},
+            order: 20,
+            type: 'operator',
+        },
+    }
+
+    for(let key in keypad) {
+        const calculatorButton = document.createElement('button');
+        calculatorButton.setAttribute('type', 'button');
+        calculatorButton.classList.add(`button-${keypad[key].type}`);
+        calculatorButton.textContent = keypad[key].value;
+        calculatorButton.style.order = keypad[key].order;
+        calculatorButton.addEventListener('click', keypad[key].action);
+        calculatorKeypad.appendChild(calculatorButton);
+    }
+};
+
+doRenderKeypad();
